a88a7b7c60c69c3beca919428bd9995b45ae0874
AL-Eldov <lednovalexsandr@gmail.com>
2023-10-25 19:29:46 +0400

    first commit

---
+++ MTC_project/Controllers/MasterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MTC_project.Controllers
+{
+    public class MasterController : Controller
+    {
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
